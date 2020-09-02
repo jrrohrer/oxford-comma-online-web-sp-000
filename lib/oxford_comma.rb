4ee7754cdd1b2f40bbe8c634f726af57c51370a2
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-09-02 15:13:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,4 @@
 def oxford_comma(array)
-  return array.join(" and ") if array.length < 3
-  array[-1] = "and " + array[-1]
+  return array.join(" and ") if array.length <= 2
   array.join(', ')
 end
